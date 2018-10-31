4f21336b6147bca1f0bd713288f03037d2efc99c
Roshchin, Roman <RRoshchin@qulix.com>
2018-10-31 20:31:55 +0300

    Bad class was added.

---
+++ src/RRR.AddIn.ApiClient/BadClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRR.AddIn.ApiClient
+{
+    public class BadClass
+    {
+        bad
+    }
+}
