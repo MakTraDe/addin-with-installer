cd1bf45a4c65cf8f19d3fe594d4cdd49953765e6
Roshchin, Roman <RRoshchin@qulix.com>
2018-11-01 14:58:22 +0300

    Add test class.
    Need to test build artefacts.

---
+++ src/RRR.AddIn.ApiClient/SimpleTestClass.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace RRR.AddIn.ApiClient
+{
+    public class SimpleTestClass
+    {
+        public void Test()
+        {
+            var client = new RestClient();
+        }
+    }
+}
